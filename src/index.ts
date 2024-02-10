bd313de742b48567a914c56c02e2ee9181cb6eba
Ramitsan <ramitsan@gmail.com>
2024-02-10 23:50:27 +0300

    add main project settings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from './app';
+import "./scss/style.scss";
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(React.createElement(App, {}, null));
